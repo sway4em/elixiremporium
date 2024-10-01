3840f94c322375fc6052f80cd4a81fb348616105
Swayam Chidrawar <schidraw@calpoly.edu>
2024-10-01 12:02:11 -0700

    Fix: dotenv leak

--- src/database.py
+++ src/database.py
@@ -4,13 +4,6 @@ from sqlalchemy import create_engine
 
 def database_connection_url():
     dotenv.load_dotenv()
-    print("-"*50)
-    print("-"*50)
-    print("reading from dotenv")
-    print("POSTGRES_URI: ", os.environ.get("POSTGRES_URI"))
-    print("-"*50)
-    print("-"*50)
-
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
