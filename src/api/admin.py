c8331fd238bccf9db6baf25414a3c10e01ec8cbe
Swayam Chidrawar <schidraw@calpoly.edu>
2024-09-30 21:53:05 -0700

    add colorama to admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,6 +4,8 @@ from src.api import auth
 import sqlalchemy
 from src import database as db
 from sqlalchemy import text
+from colorama import Fore, Style
+
 router = APIRouter(
     prefix="/admin",
     tags=["admin"],
@@ -16,10 +18,12 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    
+    print(Fore.RED + "Resetting game state" + Style.RESET_ALL)
+
     with db.engine.begin() as connection:
         connection.execute(
             text("UPDATE global_inventory SET num_green_potions = 0, num_green_ml = 0, gold = 100")
         )
+    print(Fore.RED + "Game state reset" + Style.RESET_ALL)
     return "OK"
 
