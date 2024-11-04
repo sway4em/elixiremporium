05d41d50141bdbd837bf73857919441562c62dce
Swayam Chidrawar <schidraw@calpoly.edu>
2024-11-03 16:50:35 -0800

    ledgers: catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,28 +9,25 @@ router = APIRouter()
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-    """
-    Retrieves the catalog of available potions from the inventory.
-    """
     print(Fore.RED + f"Calling get_catalog()" + Style.RESET_ALL)
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("""
             SELECT
                 r.name AS recipe_name,
-                i.stock AS quantity,
+                cpi.quantity,
                 p.rp AS price,
                 r.red,
                 r.green,
                 r.blue,
                 r.dark
-            FROM inventory i
-            JOIN recipes r ON i.potion_id = r.id
-            JOIN prices p ON i.potion_id = p.potion_id
-            WHERE i.stock > 0;
+            FROM current_potion_inventory cpi
+            JOIN recipes r ON cpi.recipe_id = r.id
+            JOIN prices p ON cpi.recipe_id = p.potion_id
+            WHERE cpi.quantity > 0;
         """)).mappings()
+
         potions = result.fetchall()
         catalog = []
-
         for potion in potions:
             recipe_name = potion['recipe_name']
             quantity = potion['quantity']
@@ -39,13 +36,10 @@ def get_catalog():
             green = potion['green']
             blue = potion['blue']
             dark = potion['dark']
-
             sku = recipe_name.lower()
             name_parts = recipe_name.lower().split('_')
             name = ' '.join(word.capitalize() for word in name_parts)
-
             potion_type = [red, green, blue, dark]
-
             catalog.append({
                 "sku": sku,
                 "name": name,
@@ -56,9 +50,11 @@ def get_catalog():
 
         if not catalog:
             print(Fore.RED + f"Inventory is empty" + Style.RESET_ALL)
+
     print(Fore.GREEN + f"Catalog retrieved: {catalog}" + Style.RESET_ALL)
     print(Fore.MAGENTA + f"API called: /catalog/ | response: {catalog}" + Style.RESET_ALL)
-    # If more than 6 unique items in catalog, return 6 random items
+
+    # if more than 6 unique items in catalog, return 6 random items
     if len(catalog) > 6:
         return random.sample(catalog, 6)
     else:
